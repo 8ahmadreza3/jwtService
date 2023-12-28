acb651163df7fe4053dc252aa9d50a68a4ad7854
8ahmadreza3 <8arhp3@gmail.com>
2023-12-28 14:28:45 +0330

    fix: auth methods

---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+const tokenService = require('../index')
+
+module.exports = (req, res, next) => {
+  if (!('authorization' in req.header)) {
+    res.status(401).send({
+      status: 401,
+      message: 'you are not authorized'
+    })
+  }
+  const token = tokenService.verify(req.header.authorization.split(' ')[1])
+  if (!token) {
+    res.status(401).send({
+      status: 401,
+      message: 'your token is not valid'
+    })
+  }
+}
